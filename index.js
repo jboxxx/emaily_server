c9e0e930c509563d60d26d28915f193e7ba6aa4b
jboxxx <jdealtrey@gmail.com>
2019-01-31 21:32:10 -0500

    updated port on index.js

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.get('/', (req, res) => {
 
 // this takes whatever port heroku wants us to run.  Would be passed into our project at runtime
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
